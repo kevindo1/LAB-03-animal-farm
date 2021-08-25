143b44cb6be9ea503c9885e8edf8c59d4d669486
Kevin Do <kevind0501@gmail.com>
2021-08-25 15:36:49 -0700

    added images and sounds

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 // import functions
+const dogSound = document.getElementById('dogSound');
+const dogImage = document.getElementById('dogImage');
 
+const catSound = document.getElementById('catSound');
+const catImage = document.getElementById('catImage');
+
+const horseSound = document.getElementById('horseSound');
+const horseImage = document.getElementById('horseImage');
 // reference needed DOM elements
 
 
@@ -7,3 +14,15 @@
   // get user input(s)
   // do any needed work with the value(s)
   // update DOM to reflect new value(s)
+
+dogImage.addEventListener('click', () => {
+    dogSound.play();
+});
+
+catImage.addEventListener('click', () => {
+    catSound.play();
+});
+
+horseImage.addEventListener('click', () => {
+    horseSound.play();
+});
