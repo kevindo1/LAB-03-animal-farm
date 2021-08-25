7c002b92dd723e2b0b1c38dac43e1b6de92a466a
Kevin Do <kevind0501@gmail.com>
2021-08-25 16:06:05 -0700

    click counter

--- app.js
+++ app.js
@@ -26,3 +26,24 @@ catImage.addEventListener('click', () => {
 horseImage.addEventListener('click', () => {
     horseSound.play();
 });
+
+document.getElementById('dogImage').addEventListener('click', updateCounter, false);
+let count = 0;
+function updateCounter() {
+    count++;
+    document.getElementById('counted').innerHTML = 'The dog has been clicked ' + count + ' times.';
+}
+
+document.getElementById('catImage').addEventListener('click', updateCatCounter, false);
+let catCount = 0;
+function updateCatCounter() {
+    catCount++;
+    document.getElementById('catCounted').innerHTML = 'The cat has been clicked ' + catCount + ' times.';
+}
+
+document.getElementById('horseImage').addEventListener('click', updateHorseCounter, false);
+let horseCount = 0;
+function updateHorseCounter() {
+    horseCount++;
+    document.getElementById('horseCounted').innerHTML = 'The horse has been clicked ' + horseCount + ' times.';
+}
